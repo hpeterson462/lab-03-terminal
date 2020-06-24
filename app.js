21478975d7af6735fb65ecab433d98d4677f2900
Heather Peterson <hpeterson462@gmail.com>
2020-06-24 16:21:14 -0700

    button color change

---
+++ app.js
@@ -0,0 +1,22 @@
+//button color change
+
+const pinkButton = document.getElementById('pink');
+
+pinkButton.addEventListener('click', () => {
+  header.style.backgroundColor = 'pink';
+})
+
+const greenButton = document.getElementById('lightGreen');
+
+greenButton.addEventListener('click', () => {
+  header.style.backgroundColor = 'lightGreen';
+})
+
+const blueButton = document.getElementById('lightBlue');
+
+blueButton.addEventListener('click', () => {
+  header.style.backgroundColor = 'lightBlue';
+})
+
+
+
