65f0eb6d0466beb03eec4f688ac2d9d2b51777a2
Heather Peterson <hpeterson462@gmail.com>
2020-06-24 16:53:59 -0700

    submit button active

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ const pinkButton = document.getElementById('pink');
 
 pinkButton.addEventListener('click', () => {
   header.style.backgroundColor = 'pink';
+  footer.style.backgroundColor = 'pink';
 })
 
 const greenButton = document.getElementById('lightGreen');
 
 greenButton.addEventListener('click', () => {
   header.style.backgroundColor = 'lightGreen';
+  footer.style.backgroundColor = 'lightGreen';
 })
 
 const blueButton = document.getElementById('lightBlue');
 
 blueButton.addEventListener('click', () => {
   header.style.backgroundColor = 'lightBlue';
+  footer.style.backgroundColor = 'lightBlue';
 })
 
+//user input name
 
+const userInput = document.getElementById('input');
 
+submitButton.addEventListener('click', () => {
+  document.getElementById('name').textContent = input.value;
+})
